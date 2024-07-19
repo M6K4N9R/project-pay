bddab458d28b560784bf83299a98ac0c0cb293ec
M6K4N9R <zubchevskiy@gmail.com>
2024-07-19 10:53:19 +0200

    Continue with setting up the database (Not today, need more time)

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,6 +21,8 @@ const links = [
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
+
+
 export default function NavLinks() {
   const pathname = usePathname();
   return (
