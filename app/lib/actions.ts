5c097d5d8f540ee27eb50de816c009da542abb27
M6K4N9R <zubchevskiy@gmail.com>
2025-01-23 21:27:17 +0100

    Add try{}catch{} blocks to actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { log } from "console";
+import { error, log } from "console";
 import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
@@ -25,11 +25,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -43,12 +46,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
