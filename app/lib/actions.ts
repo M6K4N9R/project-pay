3f57388341f694f022bc1f5de3f0e35ce86da411
M6K4N9R <zubchevskiy@gmail.com>
2025-01-23 22:48:19 +0100

    Update actions => createInvoice() with validatedFields

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,36 +32,37 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData) {
-
   // Validate form using Zod
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-  console.log("Validated Fields: ", validatedFields);
 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
-      error: validatedFields.error.flatten().fieldErrors,
+      errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing Fields. Failed to create invoice",
     };
   }
 
   // Prepare data for insertion into the database
-  const {customerId, amount, status} = validatedFields.data
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
   // Insert data into the database
   try {
     await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
   } catch (error) {
-    console.error(error);
+    // If a database error occurs, return a more specific error.
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
 
   // Revalidate the cache for the invoices page and redirect the user.
