26cbbf2c9be100fd4a9bd16939e9dc7b49056228
M6K4N9R <zubchevskiy@gmail.com>
2025-01-23 21:51:59 +0100

    Add next-lint to package.json scrpts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,6 +60,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Something went wrong')
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
